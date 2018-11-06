e2003a6c42eabd14a339e7112a572c1f8d0f0955
Llewellyn Lovell <ll@locoblox.co>
2018-11-05 23:15:33 -0600

    initial commit

--- tests/test.js
+++ tests/test.js
@@ -8,7 +8,7 @@ describe('Tests index', function() {
     index.get({ /* event */ }, { /* context */ }, (err, result) => {
       try {
         test.number(result.statusCode).is(200);
-        test.string(result.body).contains('★');
+        test.string(result.body).contains('Nunchucks');
         test.value(result).hasHeader('content-type', 'text/html');
         done();
       } catch(error) {
