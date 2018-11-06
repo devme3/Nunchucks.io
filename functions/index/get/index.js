0aede7b6b0024f9ae9c65e5a6d6b94b2ca202b20
Llewellyn Lovell <ll@locoblox.co>
2018-11-06 15:21:13 -0600

    Restructure functions

--- functions/index/get/index.js
+++ functions/index/get/index.js
@@ -1,11 +1,10 @@
 'use strict';
 
 var fs = require('fs');
-//var path = require('path');
+var path = require('path');
 
 exports.get = function(event, context, callback) {
-  //var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var contents = fs.readFileSync('./index.html');
+  var contents = fs.readFileSync(path.join(__dirname, './index.html'));
   var result = {
     statusCode: 200,
     body: contents.toString(),
